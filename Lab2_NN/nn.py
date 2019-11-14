69465cce194f07ce3217bd8ec1f5aaa613ce3c3a
[name] <tarcat21@asmsa.org>
2019-11-14 00:03:51 -0600

    added Lab 2

---
+++ Lab2_NN/nn.py
@@ -0,0 +1,18 @@
+class Node:
+	def __init__(self,weights,offset):
+		self.weights=weights
+		self.offset=offset
+		
+	def eval (self, inputList):
+		total=self.offset
+		for w,v in zip(self.weights,inputList):
+			total+=w*v
+		return 1 if total>0 else 0
+		#return total>0?1:0rr Java style 
+
+		
+ned=Node([1,1],-1)
+print(ned.eval([1,1]))
+print(ned.eval([1,0]))
+print(ned.eval([0,1]))
+print(ned.eval([0,0]))
